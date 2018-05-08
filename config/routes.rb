7635a814da99ea86a4e2cf3648616e821e78cfae
Jacob Tompkins <term171013@gmail.com>
2018-05-08 14:18:48 +0800

    finished most controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # list tasks
-  get '/tasks', to: "tasks#index"
-  # show a task
-  get '/tasks/:id', to: "tasks#show"
   # add a task
   get '/tasks/new', to: "tasks#new"
   post '/tasks', to: "tasks#create"
+  # list tasks
+  get '/tasks', to: "tasks#index"
+  # show a task
+  get '/tasks/:id', to: "tasks#show", as: :task
   # edit a task
-  get '/tasks/:id/edit', to: "tasks#edit"
-  patch '/tasks/:id/update', to: "tasks#update"
+  get '/tasks/:id/edit', to: "tasks#edit", as: :edit
+  patch '/tasks/:id', to: "tasks#update"
   # delete a task
-  delete '/tasks/:id', to: "tasks#destroy"
+  delete '/tasks/:id', to: "tasks#destroy", as: :delete
+
+  root to: 'tasks#index'
 end
