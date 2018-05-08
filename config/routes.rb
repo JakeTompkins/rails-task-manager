dd1be1073f577304e3eb3f337b894c3be3aa0954
Jacob Tompkins <term171013@gmail.com>
2018-05-08 11:55:54 +0800

    model and db created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # list tasks
+  get '/tasks', to: "tasks#index"
+  # show a task
+  get '/tasks/:id', to: "tasks#show"
+  # add a task
+  get '/tasks/new', to: "tasks#new"
+  post '/tasks', to: "tasks#create"
+  # edit a task
+  get '/tasks/:id/edit', to: "tasks#edit"
+  patch '/tasks/:id/update', to: "tasks#update"
+  # delete a task
+  delete '/tasks/:id', to: "tasks#destroy"
 end
